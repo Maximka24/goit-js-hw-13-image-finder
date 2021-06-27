7c63d1436dda7ebd66a467658a8ba105f255d25f
Kovalenko Makcim <max.kow@yandex.ru>
2021-06-27 15:19:50 +0300

    ДЗ 13
    Выполнена разметка и рендер картинок по запросу

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import NewFetchImg from './js/apiService';
+import itemCardImg from './hbs/img-card.hbs';
+
+
+
+const refs = {
+	submitForm : document.querySelector('.search-form'),
+	btnAddListImg : document.querySelector('.button-add-img'),
+	galleryItems : document.querySelector('.gallery')
+}
+
+const newFetchImg = new NewFetchImg();
+
+refs.submitForm.addEventListener('submit', onGetApiImg);
+refs.btnAddListImg.addEventListener('click', onAddNewListRender);
+
+
+function onGetApiImg(e) {
+	e.preventDefault();
+
+	clearListCardsImg();
+	newFetchImg.query = e.currentTarget.elements.query.value.trim();
+
+	if (newFetchImg.query === "") {
+		return alert('Введите наименование запроса');
+	}
+
+	newFetchImg.resetPage();
+	newFetchImg.fetchImg().then(renderCardsImg)
+	refs.btnAddListImg.classList.add('btn-block')
+}
+
+function renderCardsImg(imgs){
+	refs.galleryItems.insertAdjacentHTML('beforeend', itemCardImg(imgs));	
+}
+
+
+function onAddNewListRender() {
+	newFetchImg.fetchImg().then(renderCardsImg);
+}
+
+function clearListCardsImg(){
+	refs.galleryItems.innerHTML = "";	
+}
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,4 +1,41 @@
+const HTTP_API = 'https://pixabay.com/api/';
+const KEY_API = '22248336-3f9f08778186b55c7ac32d168';
+
+export default class NewFetchImg {
+	constructor() {
+		this.page = 1;
+		this.valueInput = '';
+	}
+
+	fetchImg() {		
+		return fetch(`${HTTP_API}?image_type=photo&orientation=horizontal&q=${this.valueInput}&page=${this.page}&per_page=12&key=${KEY_API}`)
+			.then(response => {
+				if(response.ok)return response.json();
+				throw new Error('Error fetching data')
+			})
+			.then( ({ hits }) => {
+				// console.log(data)
+				this.page += 1;
+				return hits;
+			})
+			.catch(error => {
+				console.log('Error', error)
+			})
+		
+	}
+	resetPage() {
+		this.page = 1;
+	}
+
+	get query() {
+		return this.valueInput;
+	}
+
+	set query(newValue) {
+		this.valueInput = newValue;
+	}
+}
 
 
 
-// https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=22248336-3f9f08778186b55c7ac32d168 
+
