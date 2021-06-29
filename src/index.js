aae0b47f518036eda8afdf874e3f432ecff65ee0
Kovalenko Makcim <max.kow@yandex.ru>
2021-06-29 21:17:29 +0300

    дз 13
    Добавлен скролл при нажатии на кнопку "Загрузить ещё"

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import NewFetchImg from './js/apiService';
 import itemCardImg from './hbs/img-card.hbs';
-
+import debounce from 'lodash.debounce';
 
 
 const refs = {
@@ -8,45 +8,55 @@ const refs = {
 	btnAddListImg : document.querySelector('.button-add-img'),
 	galleryItems : document.querySelector('.gallery')
 }
-
 const newFetchImg = new NewFetchImg();
 
-refs.submitForm.addEventListener('submit', onGetApiImg);
+refs.submitForm.addEventListener('submit', onGetListInApiImg);
 refs.btnAddListImg.addEventListener('click', onAddNewListRender);
+// refs.btnAddListImg.addEventListener('click', debounce(scrollToTarget, 500));
 
 
-function onGetApiImg(e) {
+function onGetListInApiImg(e) {
 	e.preventDefault();
 
 	clearListCardsImg();
 	newFetchImg.query = e.currentTarget.elements.query.value.trim();
 
 	if (newFetchImg.query === "") {
-		refs.btnAddListImg.classList.remove('btn-block')
-		return alert('Введите наименование запроса');
+		refs.btnAddListImg.classList.remove('btn-block');
+		alert(`Ошибка!!! Введите наименование запроса`);
+
+		return;
 	}
 
 	newFetchImg.resetPage();
-	newFetchImg.fetchImg().then(renderCardsImg)
-	refs.btnAddListImg.classList.add('btn-block')
+	newFetchImg.fetchImg().then(renderCardsImg);	
 }
+
+function onAddNewListRender() {	
+	newFetchImg.fetchImg().then(renderCardsImg).then(scrollToTarget);	
+}
+
 
 function renderCardsImg(imgs){
+	if (imgs.length === 0){
+		refs.btnAddListImg.classList.remove('btn-block');
+		alert(`Ошибка!!! Введите корректное наименование запроса`);
+		return;
+	}
+	
 	refs.galleryItems.insertAdjacentHTML('beforeend', itemCardImg(imgs));	
-}
-
-
-function onAddNewListRender() {
-	const element = document.getElementById('gallery');
-	
-	element.scrollIntoView({
-		behavior: 'smooth',
-		block: 'end',
-	});
-	
-	newFetchImg.fetchImg().then(renderCardsImg);
+	refs.btnAddListImg.classList.add('btn-block');
 }
 
 function clearListCardsImg(){
 	refs.galleryItems.innerHTML = "";	
 }
+
+function scrollToTarget() {    
+    refs.galleryItems.scrollIntoView({
+		behavior: "smooth",
+     	block: "end",
+    });
+}
+
+
